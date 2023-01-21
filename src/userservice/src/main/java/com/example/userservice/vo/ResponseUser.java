afbfc3baea2fa39bcd24f4bd9e4fe7e4806916ed
Jeonwoojae <dnwo0719@icloud.com>
2023-01-21 22:33:26 +0900

    [feat]:회원가입시 입력 데이터와 반환 데이터 정의

---
+++ src/userservice/src/main/java/com/example/userservice/vo/ResponseUser.java
@@ -0,0 +1,12 @@
+package com.example.userservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseUser {
+    private String email;
+    private String name;
+    private String userId;
+}
