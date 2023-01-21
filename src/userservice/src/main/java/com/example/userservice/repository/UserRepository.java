b3f212ef1784abcdca4cbff1ae475d257d7679e6
Jeonwoojae <dnwo0719@icloud.com>
2023-01-21 22:37:32 +0900

    [fix]:CrudRepository 형식 변경

--- src/userservice/src/main/java/com/example/userservice/repository/UserRepository.java
+++ src/userservice/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.userservice.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<Long, UserEntity> {
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
     UserEntity findByUserId(String userId);
     UserEntity findByEmail(String email);
 }
