6587bf4f4c6b5f545f0aedc0a76cf8b19bbaf99f
Jeonwoojae <dnwo0719@icloud.com>
2023-01-25 00:09:58 +0900

    [feat]:channel-service 프로젝트 생성

---
+++ src/channel-service/src/main/java/com/example/channelservice/ChannelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.channelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelServiceApplication.class, args);
+	}
+
+}
