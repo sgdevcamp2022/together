3ca1d9d0fcf4ef5f83e20d8471f19813e69a39c2
Jeonwoojae <dnwo0719@icloud.com>
2023-02-04 23:45:56 +0900

    [feat]:사용자 프로필 불러오기 통신만 구현

--- src/front/src/components/Profile/ProfileForm.js
+++ src/front/src/components/Profile/ProfileForm.js
@@ -1,11 +1,38 @@
+import {useContext, useRef} from "react";
+
+import AuthContext from "../../store/auth-context";
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+    const newPasswordInputRef = useRef();
+    const authCtx = useContext(AuthContext);
+
+    const submitHandler = event => {
+        event.preventDeafult();
+
+        const enteredNewPassword = newPasswordInputRef.current.value;
+
+    //    옵션 add validation
+
+        fetch('http://localhost:8000/user',
+            {method: 'GET',
+                body: JSON.stringify({
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer '+authCtx.token
+                }
+            }).then(res => {
+        //        항상 성공 응답이온다고 가정
+
+        })
+    };
   return (
+    //  TODO 받은 내용으로 유저 정보 표시하는 부분 작성 필요
     <form className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="4" ref={newPasswordInputRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
