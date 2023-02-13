fec1789fe2ff2af8796008712fc7f6e508ce3ff3
Jeonwoojae <dnwo0719@icloud.com>
2023-02-14 00:32:17 +0900

    [fix]:조회시 channel id도 전달

--- src/channel-service/src/main/java/com/example/channelservice/dto/ChannelDto.java
+++ src/channel-service/src/main/java/com/example/channelservice/dto/ChannelDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class ChannelDto {
+    private Long id;
     private String name;
     private String info;
     private Integer type;
--- src/channel-service/src/main/java/com/example/channelservice/vo/ResponseChannel.java
+++ src/channel-service/src/main/java/com/example/channelservice/vo/ResponseChannel.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class ResponseChannel {
+    private Long id;
     private String name;
     private String info;
 }
